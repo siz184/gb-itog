7886a6d83c099f2d7fdfb5cfad50f4988478e55a
Andrey <siz184@ya.ru>
2022-11-14 21:10:04 +0300

    gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] arr = new string[4]{"hello","2","world",":-)"};
+string[] newArr = new string[4];
+int n = 0;
+
+for(int i=0; i<arr.Length; i++){
+    if(arr[i].Length <= 3){
+        newArr[n] = arr[i];
+        n++;
+    }
+}
+
+for(int i=0; i<newArr.Length; i++){
+    Console.Write($"{newArr[i]} ");
+}
